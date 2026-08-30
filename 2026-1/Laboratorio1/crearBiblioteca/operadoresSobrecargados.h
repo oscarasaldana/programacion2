f3668b06223fbbc7d3c7232dfc4ec2afb6591ba9
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-08-29 19:05:00 -0500

    Add header file for overloaded operators and functions

--- 2026-1/Laboratorio1/crearBiblioteca/operadoresSobrecargados.h
+++ 2026-1/Laboratorio1/crearBiblioteca/operadoresSobrecargados.h
@@ -1,1 +1,19 @@
 
+#ifndef CREARBIBLIOTECA_PARTE1_OPERADORESSOBRECARGADOS_H
+#define CREARBIBLIOTECA_PARTE1_OPERADORESSOBRECARGADOS_H
+
+using namespace std;
+
+void cargarArchivos(ifstream &, ifstream &, ifstream &);
+void crearArchivos(ofstream &, ofstream &);
+istream & operator >> (ifstream &, struct Mascota &);
+istream & operator >> (ifstream &, struct Veterinario &);
+istream & operator >> (ifstream &, struct Atencion &);
+char *leerCadenaCaracteres(ifstream &, char );
+bool operator == (struct Atencion &, struct Atencion &);
+bool operator <= (struct Mascota &, int );
+double descomponerFecha(int , int , int , int);
+ostream & operator << (ofstream &, struct Mascota &);
+ofstream & operator << (ofstream &, struct Atencion &);
+
+#endif //CREARBIBLIOTECA_PARTE1_OPERADORESSOBRECARGADOS_H
