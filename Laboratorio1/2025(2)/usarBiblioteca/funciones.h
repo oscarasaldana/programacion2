f2e8458017b3c512fbd877d30024b136ac649804
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-01 12:34:19 -0500

    Update funciones.h

--- Laboratorio1/2025(2)/usarBiblioteca/funciones.h
+++ Laboratorio1/2025(2)/usarBiblioteca/funciones.h
@@ -1,12 +1,2 @@
 
-#ifndef USARBIBLIOTECA_PARTE2_FUNCIONES_H
-#define USARBIBLIOTECA_PARTE2_FUNCIONES_H
 
-void cargarMascotas(const char *, struct Sistema &);
-void cargarVeterinarios(const char *, struct Sistema &);
-void cargarAtenciones(const char *, struct Sistema &);
-bool existeCruce(struct Atencion *, struct Atencion &, int , int &);
-void listaMascotasAEsterelizar(const char *, struct Sistema &);
-void imprimirMascotasConAtenciones(const char *, struct Sistema &);
-
-#endif //USARBIBLIOTECA_PARTE2_FUNCIONES_H
