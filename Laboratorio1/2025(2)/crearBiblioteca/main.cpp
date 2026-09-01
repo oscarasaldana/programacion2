2bb81edda858c9ccd036730c7694693df02ff7d5
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-01 12:38:23 -0500

    Update main.cpp

--- Laboratorio1/2025(2)/crearBiblioteca/main.cpp
+++ Laboratorio1/2025(2)/crearBiblioteca/main.cpp
@@ -1,1 +1,20 @@
+#include <fstream>
 
+#include "biblioteca/operadoresSobrecargados.h"
+#include "biblioteca/Estructuras.h"
+
+using namespace std;
+
+int main() {
+
+    ifstream archAlumnos, archCursos, archMatriculas;
+    ofstream archReporte;
+
+    cargarArchivo("CarpetaDeDatos/Alumnos.csv",archAlumnos);
+    cargarArchivo("CarpetaDeDatos/Cursos.csv",archCursos);
+    cargarArchivo("CarpetaDeDatos/Matricula.csv",archMatriculas);
+    crearArchivo("CarpetaDeReportes/Reporte.txt",archReporte);
+
+
+    return 0;
+}
