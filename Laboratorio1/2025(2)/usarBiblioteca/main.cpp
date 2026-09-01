6c3678712b7f72a3d82c1e8588310f30962f4c6b
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-01 12:33:26 -0500

    Remove main function from main.cpp

--- Laboratorio1/2025(2)/usarBiblioteca/main.cpp
+++ Laboratorio1/2025(2)/usarBiblioteca/main.cpp
@@ -1,19 +1,1 @@
-#include <iostream>
 
-#include "biblioteca/veterinaria.h"
-#include "biblioteca/funciones.h"
-
-using namespace std;
-
-int main () {
-
-    struct Sistema sistema {};
-
-    cargarMascotas("CarpetaDeDatos/Mascotas.csv",sistema);
-    cargarVeterinarios("CarpetaDeDatos/Veterinarios.csv",sistema);
-    cargarAtenciones("CarpetaDeDatos/Atenciones.csv",sistema);
-    listaMascotasAEsterelizar("CarpetaDeReportes/ListaMascotasAEsterelizar.txt",sistema);
-    imprimirMascotasConAtenciones("CarpetaDeReportes/MascotasConAtenciones.txt",sistema);
-
-    return 0;
-}
