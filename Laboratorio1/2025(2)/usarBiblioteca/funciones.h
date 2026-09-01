60b4bf50163645bd05f03c9a50b57f4b61ebedad
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-01 12:41:48 -0500

    Update funciones.h

--- Laboratorio1/2025(2)/usarBiblioteca/funciones.h
+++ Laboratorio1/2025(2)/usarBiblioteca/funciones.h
@@ -1,2 +1,14 @@
 
+#ifndef USARBIBLIOTECA_PARTE2_FUNCIONES_H
+#define USARBIBLIOTECA_PARTE2_FUNCIONES_H
 
+using namespace std;
+
+void cargarAlumnos(const char *, struct Alumnos *);
+void cargarCursos(const char *, struct Cursos *);
+void cargarMatriculas(const char *, struct Cursos *, struct Alumnos *);
+char *leerCadena(ifstream &, char );
+bool encontrarCurso(struct Cursos *, char *);
+void generarReporte(const char *, struct Cursos *);
+
+#endif //USARBIBLIOTECA_PARTE2_FUNCIONES_H
