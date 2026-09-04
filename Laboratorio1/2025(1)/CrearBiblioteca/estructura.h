0fc714781009286afb448dd0ab9abdc13571549c
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-03 22:59:30 -0500

    Update estructura.h
    Added header guard and defined CadenaCaracteres structure.

--- Laboratorio1/2025(1)/CrearBiblioteca/estructura.h
+++ Laboratorio1/2025(1)/CrearBiblioteca/estructura.h
@@ -1,1 +1,11 @@
 
+#ifndef CREARBIBLIOTECA_PARTE1_ESTRUCTURA_H
+#define CREARBIBLIOTECA_PARTE1_ESTRUCTURA_H
+
+struct CadenaCaracteres {
+    char *cadena;
+    int longitud;
+    int capacidad;
+};
+
+#endif //CREARBIBLIOTECA_PARTE1_ESTRUCTURA_H
