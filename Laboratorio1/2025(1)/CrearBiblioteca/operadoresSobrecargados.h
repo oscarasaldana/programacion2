833b79710cc93623bc27ce425fd20525e9bfd75f
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-03 22:56:52 -0500

    Update operadoresSobrecargados.h

--- Laboratorio1/2025(1)/CrearBiblioteca/operadoresSobrecargados.h
+++ Laboratorio1/2025(1)/CrearBiblioteca/operadoresSobrecargados.h
@@ -1,1 +1,15 @@
 
+#ifndef USARBIBLIOTECA_PARTE2_FUNCIONES_H
+#define USARBIBLIOTECA_PARTE2_FUNCIONES_H
+
+void cargarArchivo(const char *, ifstream &);
+void crearArchivo(const char *, ofstream &);
+void cargarInfracciones(ifstream &, int *, struct CadenaCaracteres *, struct CadenaCaracteres *, int &);
+char *leerCadenaExacta(ifstream &, char );
+char *leerCadenaCaracter(ifstream &, char );
+void qsort(int *, struct CadenaCaracteres *, struct CadenaCaracteres *, int , int );
+void cambiar(int *, struct CadenaCaracteres *, struct CadenaCaracteres *, int , int );
+void reporte(ofstream &, int *, struct CadenaCaracteres *,struct CadenaCaracteres *, int );
+int contarCaracteres(char *);
+
+#endif //USARBIBLIOTECA_PARTE2_FUNCIONES_H
