9797ab5144950661f9c213c261d95d2375cc98fc
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-03 22:57:32 -0500

    Clean up main.cpp by removing unused code
    Removed unused includes and variables related to file operations and arrays.

--- Laboratorio1/2025(1)/CrearBiblioteca/main.cpp
+++ Laboratorio1/2025(1)/CrearBiblioteca/main.cpp
@@ -5,21 +5,8 @@ using namespace std;
 
 #include "biblioteca/operadoresSobrecargados.h"
 #include "biblioteca/estructura.h"
-#include "biblioteca/funciones.h"
-
-#define C 150
 
 int main() {
-    ifstream archInfraccion;
-    ofstream archReporte;
-
-    int arrDni[C], numConductores;
-    struct CadenaCaracteres arrConductores[C] {};
-    struct CadenaCaracteres arrPlacas[C] {};
-
-    cargarInfracciones(archInfraccion,arrDni,arrConductores,arrPlacas,numConductores);
-    qsort(arrDni,arrConductores,arrPlacas,0,numConductores-1);
-    reporte(archReporte,arrDni,arrConductores,arrPlacas,numConductores);
 
     return 0;
 }
