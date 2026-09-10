8c8ca429622120a2d1f12512abad4d59958c68c1
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-10 13:47:46 -0500

    Update infracciones.h

--- Laboratorio2/2025(1)/Estructuras/infracciones.h
+++ Laboratorio2/2025(1)/Estructuras/infracciones.h
@@ -1,1 +1,13 @@
 
+#ifndef SOLUCION_INFRACCIONES_H
+#define SOLUCION_INFRACCIONES_H
+
+struct Infracciones {
+    int *codigo;
+    char **descripciones;
+    char **tipos;
+    double *valores;
+    int cantidad;
+};
+
+#endif //SOLUCION_INFRACCIONES_H
