5332cee4883db2d4a92f610ceac3a75ce2c87a7e
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-10 13:47:22 -0500

    Update faltas.h

--- Laboratorio2/2025(1)/Estructuras/faltas.h
+++ Laboratorio2/2025(1)/Estructuras/faltas.h
@@ -1,1 +1,12 @@
 
+#ifndef SOLUCION_FALTAS_H
+#define SOLUCION_FALTAS_H
+
+struct Faltas {
+    struct Conductores conductores {};
+    struct Infracciones infracciones {};
+    char ***placa {};
+    int *capacidades {};
+};
+
+#endif //SOLUCION_FALTAS_H
