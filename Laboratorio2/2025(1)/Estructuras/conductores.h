7e4ff09a741044503bfa843907b86f081d4a537d
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-10 13:46:58 -0500

    Update conductores.h

--- Laboratorio2/2025(1)/Estructuras/conductores.h
+++ Laboratorio2/2025(1)/Estructuras/conductores.h
@@ -1,1 +1,11 @@
 
+#ifndef SOLUCION_CONDUCTORES_H
+#define SOLUCION_CONDUCTORES_H
+
+struct Conductores {
+    int *dnis;
+    char **nombres;
+    int cantidad;
+};
+
+#endif //SOLUCION_CONDUCTORES_H
