da5039e765a63611885f30c556fef8a4b433656d
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-13 17:02:26 -0500

    Update main.cpp
    Initialize main function to handle book inventory and orders.

--- Laboratorio2/2024(2)/main.cpp
+++ Laboratorio2/2024(2)/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+using namespace std;
+
+#include "biblioteca/funciones.h"
+
+int main() {
+
+    int **inventario, **pedidosClientes;
+    char ***libros, ***pedidosLibros;
+    bool **pedidosAtendidos;
+
+    lecturaDeLibros("CarpetaDeDatos/Libros.csv",libros,inventario);
+    pruebaDeLecturaDeLibros("CarpetaDeReportes/ReporteDeLibrosInicial.txt",libros,inventario);
+
+    atencionDePedidos("CarpetaDeDatos/Pedidos.txt",libros,inventario,pedidosClientes,
+                     pedidosLibros,pedidosAtendidos);
+
+    reporteDeEntregaDePedidos("CarpetaDeReportes/ReporteDeEntregaDePedidos.txt",pedidosClientes,
+                              pedidosLibros,pedidosAtendidos);
+
+    return 0;
+}
