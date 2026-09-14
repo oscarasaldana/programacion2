e1249e73412ab28d3c8865512d506e5ad9523541
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 10:53:30 -0500

    Update main.cpp

--- Laboratorio2/2025(1)/main.cpp
+++ Laboratorio2/2025(1)/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+#include "biblioteca/funciones.h"
+#include "estructuras/conductores.h"
+#include "estructuras/faltas.h"
+#include "estructuras/infracciones.h"
+
+int main() {
+
+    struct Faltas faltas {};
+
+    cargarFaltasConductores("CarpetaDeDatos/Conductores.csv","CarpetaDeDatos/Infracciones.csv",
+                            "CarpetaDeDatos/Faltas.csv",faltas);
+    imprimirFaltasConductores("CarpetaDeReportes/ReporteDeConductores.txt",faltas);
+
+    return 0;
+}
