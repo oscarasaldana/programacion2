52c5151ee0117b867209c74937ea02d48c79a94a
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 10:53:45 -0500

    Update funciones.h

--- Laboratorio2/2025(1)/biblioteca/funciones.h
+++ Laboratorio2/2025(1)/biblioteca/funciones.h
@@ -1,1 +1,17 @@
 
+#ifndef CODIGO_FUNCIONES_H
+#define CODIGO_FUNCIONES_H
+
+void cargarArchivo(const char *, ifstream &);
+void crearArchivo(const char *, ofstream &);
+void operator += (struct Conductores &, const char *);
+char *leerCadenaCaracteres(ifstream &, char );
+void operator += (struct Infracciones &, const char *);
+void cargarFaltasConductores(const char *, const char *, const char *, struct Faltas &);
+int encontrarPosicionDni(const int *, int , int );
+void cargarPlacas(struct Faltas &, char *, int );
+bool placaRepetida(char **, char *);
+void incrementarCapacidad(char **&, int &);
+void imprimirFaltasConductores(const char *, struct Faltas &);
+
+#endif //CODIGO_FUNCIONES_H
