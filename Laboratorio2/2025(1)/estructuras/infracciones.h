df5cb709223c4fcd28db9cec194d8bdce6a242f2
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 10:56:27 -0500

    Update infracciones.h
    Added header guard and defined Infracciones structure.

--- Laboratorio2/2025(1)/estructuras/infracciones.h
+++ Laboratorio2/2025(1)/estructuras/infracciones.h
@@ -1,1 +1,12 @@
 
+#ifndef CODIGO_INFRACCIONES_H
+#define CODIGO_INFRACCIONES_H
+
+struct Infracciones {
+    int *codigo;
+    char **descripcion;
+    char **tipos;
+    int cantidad;
+};
+
+#endif //CODIGO_INFRACCIONES_H
