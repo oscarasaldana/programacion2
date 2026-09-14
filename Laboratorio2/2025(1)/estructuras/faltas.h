cc38707eebab3f5db15124f768a42af779e9775d
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 10:56:08 -0500

    Update faltas.h

--- Laboratorio2/2025(1)/estructuras/faltas.h
+++ Laboratorio2/2025(1)/estructuras/faltas.h
@@ -1,1 +1,15 @@
 
+#ifndef CODIGO_FALTAS_H
+#define CODIGO_FALTAS_H
+#include "conductores.h"
+#include "infracciones.h"
+
+struct Faltas {
+    Conductores conductores;
+    Infracciones infracciones;
+
+    char ***placas;
+    int *capacidad;
+};
+
+#endif //CODIGO_FALTAS_H
