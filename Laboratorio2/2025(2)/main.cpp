e00b3e67ad28b571de0b08d6cdb3a665a2c9998e
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-17 11:33:10 -0500

    Update main.cpp

--- Laboratorio2/2025(2)/main.cpp
+++ Laboratorio2/2025(2)/main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+#include "biblioteca/funciones.h"
+
+int main() {
+
+    char ***categorias {};
+    cargarCategorias("CarpetaDeDatos/categorias.csv",categorias);
+
+    char ***streamers {};
+    int **fechasPromedios {};
+    long long **tiempoRepSeguidores {};
+    cargarStreamers("CarpetaDeDatos/streamers.csv",streamers,fechasPromedios,tiempoRepSeguidores);
+
+    char ***comentarios {};
+    char ***etiquetas {};
+    cargarEtiquetasComentarios("CarpetaDeDatos/comentarios.csv",comentarios,etiquetas);
+
+    imprimirReportes("CarpetaDeReportes/reporte.txt",categorias,streamers,fechasPromedios,tiempoRepSeguidores,
+                     comentarios,etiquetas);
+
+    return 0;
+}
