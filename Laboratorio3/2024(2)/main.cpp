2d8a05debc34e40554b9819dcf7ad3a06fa4a498
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-25 13:02:39 -0500

    Update main.cpp

--- Laboratorio3/2024(2)/main.cpp
+++ Laboratorio3/2024(2)/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+#include "biblioteca/funciones.h"
+
+int main() {
+
+    void *inventario, *menu;
+
+    cargarInventario("CarpetaDeDatos/inventario.csv",inventario);
+    probarCargaInventario("CarpetaDeReportes/reporteInventario.txt",inventario);
+    cargarMenu("CarpetaDeDatos/menu.csv",menu);
+    probarCargaMenu("CarpetaDeReportes/reporteMenu.txt",menu);
+    actualizarMenu("CarpetaDeDatos/insumosBebidas.csv",inventario,menu);
+    reporteMenu("CarpetaDeReportes/reporteFinal.txt",menu);
+
+    return 0;
+}
