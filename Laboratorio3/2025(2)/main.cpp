5870c37a3ea92287a03fa14bde24c620f175f0ff
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-25 13:00:37 -0500

    Update main.cpp

--- Laboratorio3/2025(2)/main.cpp
+++ Laboratorio3/2025(2)/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+#include "biblioteca/funciones.h"
+
+int main() {
+
+    void *streamers, *comentarios;
+
+    cargarStreamers(streamers);
+    cargarComentarios(comentarios);
+    actualizarComentarios(streamers,comentarios);
+    imprimirStreamers(streamers);
+
+    return 0;
+}
