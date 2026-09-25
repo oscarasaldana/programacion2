664983315c727fa01f55409688e1d5a298a7c202
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-25 13:04:35 -0500

    Update main.cpp

--- Laboratorio3/2024(1)/main.cpp
+++ Laboratorio3/2024(1)/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+#include "biblioteca/funciones.h"
+
+int main() {
+
+    void *clientes, *reserva;
+
+    cargarClientes(clientes);
+    crearReserva(reserva);
+    cargarReservas(clientes,reserva);
+    reporteFinal(reserva);
+
+    return 0;
+}
