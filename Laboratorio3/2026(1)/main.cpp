f49a103b59f360361c1bf6046ea1eee185c00fdf
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-25 12:57:59 -0500

    Update main.cpp

--- Laboratorio3/2026(1)/main.cpp
+++ Laboratorio3/2026(1)/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+#include "biblioteca/funciones.h"
+
+int main() {
+
+    void *pacientes;
+
+    cargarPacientes("CarpetaDeDatos/pacientes.csv",pacientes);
+    cargarVisitas("CarpetaDeDatos/visitas.csv",pacientes);
+    generarReportes("CarpetaDeReportes/reportes.txt",pacientes);
+
+    return 0;
+}
