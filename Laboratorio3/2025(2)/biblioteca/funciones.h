07b99ea68a0eedd11c6b64a57251a6295cb7d1e9
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-25 13:01:42 -0500

    Update funciones.h

--- Laboratorio3/2025(2)/biblioteca/funciones.h
+++ Laboratorio3/2025(2)/biblioteca/funciones.h
@@ -1,1 +1,18 @@
+#ifndef CODIGO_FUNCIONES_H
+#define CODIGO_FUNCIONES_H
 
+void cargarArchivo(const char *, ifstream &);
+void crearArchivo(const char *, ofstream &);
+void cargarStreamers(void *&);
+char *leerCadenaCaracteres(ifstream &, char );
+void incrementarCapacidadStreamers(void ***&, int , int &);
+void cargarDatosStreamer(void **&, char *, long long , char *);
+void cargarComentarios(void *&);
+void incrementarCapacidadComentarios(void ***&, int , int &);
+void cargarDatosComentarios(void **&, char *, char *, char *, char *);
+void actualizarComentarios(void *&, void *);
+void cargarComentarioStreamer(char ***&, char *, char *, int &, int &);
+void imprimirStreamers(void *);
+void imprimirDatosStreamers(ofstream &, void **);
+
+#endif //CODIGO_FUNCIONES_H
