060340036b2089d8b8fc7f91daf632bf9379df4f
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-25 13:03:58 -0500

    Update funciones.h

--- Laboratorio3/2024(2)/biblioteca/funciones.h
+++ Laboratorio3/2024(2)/biblioteca/funciones.h
@@ -1,1 +1,27 @@
 
+#ifndef CODIGO_FUNCIONES_H
+#define CODIGO_FUNCIONES_H
+
+void cargarArchivo(const char *, ifstream &);
+void crearArchivo(const char *, ofstream &);
+void cargarInventario(const char *, void *&);
+char *leerCadenaCaracteres(ifstream &, char );
+void cargarDatosInventario(void **&, char *, char *, double , double , char *);
+void probarCargaInventario(const char *, void *&);
+void mostrarInventario(ofstream &, void **&);
+void incrementarCapacidadInventario(void ***&, int , int &);
+void cargarMenu(const char *, void *&);
+void incrementarCapacidadMenu(void ***&, int , int &);
+void cargarDatosMenu(void **&, char *, char *, char *, char , bool , double , int );
+void probarCargaMenu(const char *, void *&);
+void mostrarMenu(ofstream &, void **&);
+void actualizarMenu(const char *, void *&, void *&);
+int encontrarPosicionBebida(void ***, char *);
+void cargarDatosInsumosBebida(void **&, int &, int &, void *&, void *&,char *, int , char , int );
+void **informacionInsumo(void ***, char *, int &);
+void actualizarDatosInsumosYBebidas(void *&, void *&, int , int , int );
+void reporteMenu(const char *, void *&);
+void mostrarBebidas(ofstream &, void **&);
+void mostrarTipoBebida(ofstream &, char *);
+
+#endif //CODIGO_FUNCIONES_H
